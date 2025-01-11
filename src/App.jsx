1968d0da71f02db33aad8c9b462a15b19c02d9f7
Mitka92 <134132348+Mitka92@users.noreply.github.com>
2025-01-11 16:01:28 +0200

    add lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,44 @@
 import './App.css';
+import { lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage.jsx';
-import CatalogPage from './pages/CatalogPage/CatalogPage.jsx';
-import CamperDetailsPage from './pages/CamperDetailsPage/CamperDetailsPage.jsx';
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage.jsx';
 import Header from './components/header/Header.jsx';
-import CamperFeatures from './components/CamperFeatures/CamperFeatures.jsx';
-import CamperReviews from './components/CamperReviews/CamperReviews.jsx';
 import { Toaster } from 'react-hot-toast'; // Імпортуємо Toaster
+import Loader from './components/Loader/Loader.jsx';
+
+// Ліниве завантаження компонентів
+const HomePage = lazy(() => import('./pages/HomePage/HomePage.jsx'));
+const CatalogPage = lazy(() => import('./pages/CatalogPage/CatalogPage.jsx'));
+const CamperDetailsPage = lazy(() =>
+  import('./pages/CamperDetailsPage/CamperDetailsPage.jsx')
+);
+const NotFoundPage = lazy(() =>
+  import('./pages/NotFoundPage/NotFoundPage.jsx')
+);
+const CamperFeatures = lazy(() =>
+  import('./components/CamperFeatures/CamperFeatures.jsx')
+);
+const CamperReviews = lazy(() =>
+  import('./components/CamperReviews/CamperReviews.jsx')
+);
+
 function App() {
   return (
     <>
-      <Header />
       <Toaster />
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/catalog" element={<CatalogPage />} />
-          <Route path="/catalog/:id" element={<CamperDetailsPage />}>
-            <Route path="/catalog/:id/features" element={<CamperFeatures />} />
-            <Route path="/catalog/:id/reviews" element={<CamperReviews />} />
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </main>
+      <Suspense fallback={<Loader />}>
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/catalog" element={<CatalogPage />} />
+            <Route path="/catalog/:id" element={<CamperDetailsPage />}>
+              <Route path="features" element={<CamperFeatures />} />
+              <Route path="reviews" element={<CamperReviews />} />
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </main>
+      </Suspense>
     </>
   );
 }
